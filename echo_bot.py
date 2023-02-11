f7916ed33182c3c20457d58419ff86b9636bbd36
Anonymoushjk <97667119+Anonymoushjk@users.noreply.github.com>
2023-02-11 20:25:20 +0600

    Update echo_bot.py

--- echo_bot.py
+++ echo_bot.py
@@ -1,3 +1,3 @@
 import telebot
 
-bot = telebot.TeleBot("TOKEN", parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
+bot = telebot.TeleBot("6236847085:AAEwZFAUmX1YBk53gPa-A9zc3uOzrb2iqpg", parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
